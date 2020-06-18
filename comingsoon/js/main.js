24b2280fb7035e99cff44ab8e821dcc9ccfe004d
varntech <info@varn.tech>
2020-06-19 05:11:23 +0900

    comingsoon4

--- comingsoon/js/main.js
+++ comingsoon/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "February 27, 2020 12:00:00",
+	date: "July 31, 2020 12:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
