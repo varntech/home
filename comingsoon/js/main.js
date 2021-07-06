79d1c889b0034a350e4f41fb133763189abe3722
snohw <snohw@fairappl.org>
2021-07-07 01:31:31 +0900

    comingsoon9 (fixed)

--- comingsoon/js/main.js
+++ comingsoon/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "Aug 1, 2021 12:00:00",
+	date: "August 1, 2021 12:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
